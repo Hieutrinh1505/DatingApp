54fe8de57dbcf32fceef7c9861fe11793a1c073a
Hieutrinh1505 <hieutrinh358@gmail.com>
2021-01-26 17:22:19 -0600

    Update

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,36 +2,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    
+    [Authorize]
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
-        public UsersController(DataContext context)
+        private readonly IMapper _mapper;
+
+        private readonly IUserRepository _userRepository;
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
+
         }
 
         [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() // simple iteration over a collection of a specified type
+
+        public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() // simple iteration over a collection of a specified type
         {
-            return await _context.Users.ToListAsync();// Async
+            var users = await _userRepository.GetMembersAsync();
+           
+            return Ok(users);
+
 
         }
         //api/users/index. Get data by Id
-        [Authorize]
-        [HttpGet("{id}")]
-        
-        public async Task<ActionResult<AppUser>> GetUser(int id) // simple iteration over a collection of a specified type
+
+        [HttpGet("{username}")]
+
+        public async Task<ActionResult<MemberDto>> GetUser(string username) // simple iteration over a collection of a specified type
         {
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetMemberAsync(username);
 
         }
     }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
@@ -12,7 +14,8 @@ import { AccountService } from '../_services/account.service';
 export class NavComponent implements OnInit {
   model: any = {}
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router,
+              private toastr: ToastrService) { }
 
   
 
@@ -23,10 +26,8 @@ export class NavComponent implements OnInit {
   
   login() {
     this.accountService.login(this.model).subscribe(response => {
-      console.log(response);
+      this.router.navigateByUrl('/members');
       
-    }, error => {
-      console.log(error);
     })
   }
 
